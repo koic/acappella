603690168303a9fb55322c364cf0c6ca048ad219
Koichi ITO <koic.ito@gmail.com>
2014-12-13 14:03:02 +0900

    extract option parser class

---
+++ lib/acappella/option_parser.rb
@@ -0,0 +1,21 @@
+require 'optparse'
+
+module ACappella
+  class OptionParser
+    def self.parse(argv)
+      opt = ::OptionParser.new
+
+      options = {
+        host: 'localhost',
+        port: '8989'
+      }
+
+      opt.on('-h', '--host=VAL') {|v| options[:host] = v }
+      opt.on('-p', '--port=VAL') {|v| options[:port] = v }
+
+      opt.parse!(argv)
+
+      options
+    end
+  end
+end
