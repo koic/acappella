00c0520e4a7fa0538f342ea71aa87ff9f8e02c19
Koichi ITO <koic.ito@gmail.com>
2014-12-13 15:38:42 +0900

    ASCII order

--- lib/acappella.rb
+++ lib/acappella.rb
@@ -1,8 +1,8 @@
 module ACappella; end
 
 require 'acappella/client'
+require 'acappella/option_parser'
 require 'acappella/server'
 require 'acappella/set_list'
 require 'acappella/singer'
 require 'acappella/songwriter'
-require 'acappella/option_parser'
