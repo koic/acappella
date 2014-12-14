20d656809e4106f05ed7f2eb05d495b5558f8a2e
Koichi ITO <koic.ito@gmail.com>
2014-12-14 23:05:07 +0900

    IJWTK

--- lib/acappella/singer.rb
+++ lib/acappella/singer.rb
@@ -1,7 +1,17 @@
 module ACappella
   class Singer
     def sing(lyrics)
+      logging(lyrics)
+
       system('say', lyrics)
+    end
+
+    private
+
+    def logging(lyrics)
+      sung_at = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+
+      puts [sung_at, lyrics].join("\t")
     end
   end
 end
