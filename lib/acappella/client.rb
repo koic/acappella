ed355e72b9a94050688f0484badf3e5de3716080
Koichi ITO <koic.ito@gmail.com>
2014-12-14 22:43:00 +0900

    add tips information

--- lib/acappella/client.rb
+++ lib/acappella/client.rb
@@ -10,6 +10,7 @@ module ACappella
       uri = URI.parse("druby://#{host}:#{port}").to_s
 
       puts "Connect to #{uri}"
+      puts 'Run `acappella-client --help` for more startup options'
 
       @songwriter = DRb::DRbObject.new_with_uri(uri)
     end
